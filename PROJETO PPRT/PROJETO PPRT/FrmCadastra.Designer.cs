05030eb6a1356fc30ce2f66e66734d5263098ac1
kklebson <135077489+kklebson@users.noreply.github.com>
2023-08-29 21:24:57 -0300

    cadastro

--- PROJETO PPRT/PROJETO PPRT/FrmCadastra.Designer.cs
+++ PROJETO PPRT/PROJETO PPRT/FrmCadastra.Designer.cs
@@ -28,12 +28,229 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Panel panel1;
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtproduto = new System.Windows.Forms.TextBox();
+            this.txtquantidade = new System.Windows.Forms.TextBox();
+            this.txtpreco = new System.Windows.Forms.TextBox();
+            this.btncadastrar = new System.Windows.Forms.Button();
+            this.btneditar = new System.Windows.Forms.Button();
+            this.btnfoto = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnvolta = new System.Windows.Forms.Button();
+            panel1 = new System.Windows.Forms.Panel();
+            panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SandyBrown;
+            this.label1.Location = new System.Drawing.Point(22, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PRODUTO:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SandyBrown;
+            this.label2.Location = new System.Drawing.Point(22, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "QUANTIDADE:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.SandyBrown;
+            this.label3.Location = new System.Drawing.Point(22, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "PREÇO:";
+            // 
+            // txtproduto
+            // 
+            this.txtproduto.Location = new System.Drawing.Point(157, 95);
+            this.txtproduto.Name = "txtproduto";
+            this.txtproduto.Size = new System.Drawing.Size(172, 20);
+            this.txtproduto.TabIndex = 3;
+            // 
+            // txtquantidade
+            // 
+            this.txtquantidade.Location = new System.Drawing.Point(157, 132);
+            this.txtquantidade.Name = "txtquantidade";
+            this.txtquantidade.Size = new System.Drawing.Size(172, 20);
+            this.txtquantidade.TabIndex = 4;
+            // 
+            // txtpreco
+            // 
+            this.txtpreco.Location = new System.Drawing.Point(157, 171);
+            this.txtpreco.Name = "txtpreco";
+            this.txtpreco.Size = new System.Drawing.Size(172, 20);
+            this.txtpreco.TabIndex = 5;
+            // 
+            // btncadastrar
+            // 
+            this.btncadastrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btncadastrar.FlatAppearance.BorderSize = 0;
+            this.btncadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btncadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncadastrar.ForeColor = System.Drawing.Color.SandyBrown;
+            this.btncadastrar.Location = new System.Drawing.Point(16, 251);
+            this.btncadastrar.Name = "btncadastrar";
+            this.btncadastrar.Size = new System.Drawing.Size(104, 51);
+            this.btncadastrar.TabIndex = 6;
+            this.btncadastrar.Text = "Cadastrar";
+            this.btncadastrar.UseVisualStyleBackColor = true;
+            // 
+            // btneditar
+            // 
+            this.btneditar.FlatAppearance.BorderSize = 0;
+            this.btneditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btneditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btneditar.ForeColor = System.Drawing.Color.SandyBrown;
+            this.btneditar.Location = new System.Drawing.Point(136, 251);
+            this.btneditar.Name = "btneditar";
+            this.btneditar.Size = new System.Drawing.Size(112, 51);
+            this.btneditar.TabIndex = 7;
+            this.btneditar.Text = "Editar";
+            this.btneditar.UseVisualStyleBackColor = true;
+            // 
+            // btnfoto
+            // 
+            this.btnfoto.FlatAppearance.BorderSize = 0;
+            this.btnfoto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnfoto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnfoto.ForeColor = System.Drawing.Color.SandyBrown;
+            this.btnfoto.Location = new System.Drawing.Point(254, 251);
+            this.btnfoto.Name = "btnfoto";
+            this.btnfoto.Size = new System.Drawing.Size(118, 51);
+            this.btnfoto.TabIndex = 8;
+            this.btnfoto.Text = "Inserir Foto";
+            this.btnfoto.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            panel1.CausesValidation = false;
+            panel1.Controls.Add(this.btnvolta);
+            panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            panel1.Location = new System.Drawing.Point(0, 0);
+            panel1.Margin = new System.Windows.Forms.Padding(0);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(800, 46);
+            panel1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(16)))), ((int)(((byte)(3)))));
+            this.panel2.Controls.Add(this.dateTimePicker1);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.btneditar);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.btnfoto);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.btncadastrar);
+            this.panel2.Controls.Add(this.txtproduto);
+            this.panel2.Controls.Add(this.txtpreco);
+            this.panel2.Controls.Add(this.txtquantidade);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 46);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(398, 404);
+            this.panel2.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(430, 89);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(341, 300);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.SandyBrown;
+            this.label5.Location = new System.Drawing.Point(22, 204);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "VALIDADE:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(157, 203);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(172, 20);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // btnvolta
+            // 
+            this.btnvolta.FlatAppearance.BorderSize = 0;
+            this.btnvolta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnvolta.ForeColor = System.Drawing.Color.SandyBrown;
+            this.btnvolta.Location = new System.Drawing.Point(548, 3);
+            this.btnvolta.Name = "btnvolta";
+            this.btnvolta.Size = new System.Drawing.Size(130, 32);
+            this.btnvolta.TabIndex = 0;
+            this.btnvolta.Text = "GITCAKE";
+            this.btnvolta.UseVisualStyleBackColor = true;
+            // 
+            // FrmCadastra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(16)))), ((int)(((byte)(3)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(panel1);
+            this.Name = "FrmCadastra";
             this.Text = "FrmCadastra";
+            this.Load += new System.EventHandler(this.FrmCadastra_Load);
+            panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtproduto;
+        private System.Windows.Forms.TextBox txtquantidade;
+        private System.Windows.Forms.TextBox txtpreco;
+        private System.Windows.Forms.Button btncadastrar;
+        private System.Windows.Forms.Button btneditar;
+        private System.Windows.Forms.Button btnfoto;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnvolta;
     }
 }
